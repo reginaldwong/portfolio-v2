b4ae661e7996e99d0d664329e80c13c31c3a336b
Reginald Wong <reginald_wong@outlook.com>
2022-01-28 13:03:26 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+VANTA.FOG({
+  el: '#styled-header',
+  THREE,
+  mouseControls: false,
+  touchControls: false,
+  gyroControls: false,
+  highlightColor: 0x0bb0b1,
+  midtoneColor: 0xe3bf98,
+  lowlightColor: 0x13848c,
+  baseColor: 0xfce9e9,
+  blurFactor: 0.6,
+  speed: 3,
+  zoom: 1.5,
+});
